d01270051dc34332a86609841a50ef7cea6b028a
Sumin Ivan <sumin.ivan01@gmail.com>
2025-09-05 07:30:25 +1000

    Создать проект с Cookiecutter-django

---
+++ remi_shop/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from remi_shop.users.models import User
+from remi_shop.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
